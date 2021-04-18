2dbabbe0d29c825bcccb52228c24a8f47491e283
Alshekarchee <alielshekarchee@gmail.com>
2021-04-18 16:59:27 +0300

    authorization added

--- config/auth.js
+++ config/auth.js
@@ -1,20 +1,32 @@
-// const User = require("../models/User");
+const User = require("../models/User");
 
 module.exports = {
-  ensureAuthenticated: function(req, res, next) {
+  ensureAuthenticated: function (req, res, next) {
     if (req.isAuthenticated()) {
       return next();
     }
     req.flash('error_msg', 'Please log in to view that resource');
     res.redirect('/users/login');
   },
-  forwardAuthenticated: function(req, res, next) {
+  forwardAuthenticated: function (req, res, next) {
     if (!req.isAuthenticated()) {
       return next();
-    }     
-    res.redirect('/dashboard');      
+    }
+    res.redirect('/dashboard');
+  },
+  ensureAdmin: function (req, res, next) {
+    User.findOne({ email: req.user.email }).then(user => {
+      // console.log(`req.email: ${req.user.email}, and user.email: ${user.email}, and user.role: ${user.role}`);      
+      if (user.role === 'god') {
+        console.log(`you are authorized, you are a ${user.role}`);
+        return next();
+      } else {
+        // res.status(403);
+        res.redirect('/users/login');
+        console.log(`you are not authorized, you are a ${user.role}`);
+      }
+    }).catch(err => console.log(err));
   }
 };
 
 
-// User.findOne(req.email).then(user => {user.role === 'god' ? res.redirect('/admindashboard') : res.redirect('/dashboard');}).catch(err => console.log(err));   
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 // Load User model
 const User = require('../models/User');
-const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const { ensureAuthenticated, forwardAuthenticated, ensureAdmin } = require('../config/auth');
 const { JsonWebTokenError } = require('jsonwebtoken');
 
 // Welcome Page
@@ -19,15 +19,18 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
   }).catch(err => console.log(err));
 });
 
-// // Admin Dashboard
-// router.get('/admindashboard', ensureAuthenticated, (req, res) => {
-//   const token = jwt.sign(req.user.email, 'top-secret');
-//   res.render('dashboard', { user: req.user });
-//   User.findOne({ email: req.user.email }).then(user => {
-//     user.token = token;
-//     user.save().then().catch(err => console.log(err));
-//   }).catch(err => console.log(err));
-// });
+// Admin Dashboard
+router.get('/admindashboard', ensureAuthenticated, ensureAdmin, (req, res) => {
+  const token = jwt.sign(req.user.email, 'top-secret');
+  User.find().then(users => {
+    res.render('admindashboard', { user: req.user, users: users });
+  }).catch(err => console.log(err));
+  
+  User.findOne({ email: req.user.email }).then(user => {
+    user.token = token;
+    user.save().then().catch(err => console.log(err));
+  }).catch(err => console.log(err));
+});
 
 // Websocket
 router.get('/ws', ensureAuthenticated, (req, res) => {
